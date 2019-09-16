42eb12c0661342e9cb9bda97dd38437aa80e5c38
kaushal-king <kaushaljethava12@gmail.com>
2019-09-16 19:18:14 +0530

    Initial commit

---
+++ app/src/main/java/com/appsnipp/loginsamples/registration/forgotpassword_activity.java
@@ -0,0 +1,32 @@
+package com.appsnipp.loginsamples.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.appsnipp.loginsamples.R;
+
+public class forgotpassword_activity extends AppCompatActivity implements TextWatcher {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpassword_activity);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
---
+++ app/src/main/java/com/appsnipp/loginsamples/registration/password.java
@@ -0,0 +1,13 @@
+package com.appsnipp.loginsamples.registration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appsnipp.loginsamples.R;
+
+public class password extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_password_activity);
+    }
+}
